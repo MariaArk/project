43688859983c4ce0977493f11a86573c1af9de9f
MariaArk <pineapple.maria.7@gmail.com>
2024-06-16 23:05:34 +0300

    3

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    //<App />
+    <div>Good job!
+        <button>Button</button>     
+    </div>
+    
 );
