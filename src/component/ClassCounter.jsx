e041789ad71028d0983430d7ab936d4d8fb4df6e
MariaArk <pineapple.maria.7@gmail.com>
2024-06-16 23:30:37 +0300

    9

--- src/component/ClassCounter.jsx
+++ src/component/ClassCounter.jsx
@@ -1,22 +1,20 @@
 import React from "react";
 
 class ClassCounter  extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {count: 0}
+    }
+        
+    increment() {
+        this.setState({count: this.state.count +1})
+    }
 
+    decrement() {
+        this.setState({count: this.state.count -1})
+    }
+    
     render(){
-
-        constructor(props) {
-            super(props);
-            this.state = {count: 0}
-        }
-            
-        increment() {
-            this.setState({count: this.state.count +1})
-        }
-
-        decrement() {
-            this.setState({count: this.state.count -1})
-        }
-
         return (
             <div>
                 <h1>{this.count}</h1>
